981248c4f06b85ef56e4391947d500919157660f
Igor Mukhin <igor.mukhin@gmail.com>
2015-09-01 14:28:07 +0300

    Moved: Routes test from common to app

--- phpunit/app/ConventionsTest.php
+++ phpunit/app/ConventionsTest.php
@@ -14,6 +14,29 @@ class ConventionsTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     *
+     */
+    public function testRoutesExists()
+    {
+        $filename = sprintf("%s/app/config/routes.yml", $this->rootPath);
+        $this->assertFileExists($filename, 'File "app/config/routes.yml" does not exists');
+    }
+
+    /**
+     * @depends testRoutesExists
+     */
+    public function testRoutes()
+    {
+        $filename = sprintf("%s/app/config/routes.yml", $this->rootPath);
+        $content = file_get_contents($filename);
+        $yaml = Yaml::parse($content);
+
+        foreach ($yaml as $route_name=>$route_parameters) {
+            $this->assertTrue($route_parameters['path'] == '/' || '/' != substr(trim($route_parameters['path']), -1), 'Never use a closing slash on routes (except top-level "/" path)');
+        }
+    }
+
+    /**
      * Application MUST have composer.lock in repository.
      * So composer.lock CAN'T BE at .gitignore file.
      */
